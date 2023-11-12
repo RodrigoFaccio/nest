c9e0c2dd128e48b2a4b6ea2f2d9a1e032e0a9143
RodrigoFaccio <rodrigo.facio@adminyogha.com.br>
2023-11-12 13:39:50 -0300

    first commit

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+
+describe('User entitys unit tests', () => {
+    let props: UserProps;
+    let sut: UserEntity;
+    beforeEach(() => {
+        props = {
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        };
+        sut = new UserEntity(props);
+    });
+
+    it('should constructor methos ', () => {
+        expect(sut.props.name).toEqual(props.name);
+        expect(sut.props.email).toEqual(props.email);
+        expect(sut.props.password).toEqual(props.password);
+        expect(sut.props.createdAt).toBeInstanceOf(Date);
+    });
+
+    it('should getter of name field', () => {
+        expect(sut.props.name).toBeDefined();
+        expect(sut.props.name).toEqual(props.name);
+        expect(typeof sut.props.name).toBe('string');
+    });
+
+    it('should getter of email field', () => {
+        expect(sut.props.email).toBeDefined();
+        expect(sut.props.email).toEqual(props.email);
+        expect(typeof sut.props.email).toBe('string');
+    });
+    it('should getter of password field', () => {
+        expect(sut.props.password).toBeDefined();
+        expect(sut.props.password).toEqual(props.password);
+        expect(typeof sut.props.password).toBe('string');
+    });
+
+    it('should getter of createdAt field', () => {
+        expect(sut.props.createdAt).toBeDefined();
+        expect(sut.props.createdAt).toBeInstanceOf(Date);
+    });
+});
